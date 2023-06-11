6d2df9810a8b52d58402a2b34beb1126e64356f2
clover <threeleafclover9@gmail.com>
2023-06-11 22:42:03 +0900

    Refactor association mapping foundation

--- src/main/java/inflearn/jpabasic/domain/Item.java
+++ src/main/java/inflearn/jpabasic/domain/Item.java
@@ -7,9 +7,11 @@ import javax.persistence.Id;
 
 @Entity
 public class Item {
+
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
     private Long id;
+
     private String name;
     private int price;
     private int stockQuantity;
